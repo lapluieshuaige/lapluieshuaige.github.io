8d3ab85dcf1e7da75100070db9fb3c54d85b93f3
lapluieshuaige <564909588@qq.com>
2016-07-12 16:46:26 +0800

    hiho

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,12 +11,12 @@ if ($(window).width() <= 1280) {
 }
 
 // Variables
-    tag1       = $('.pl__all'),
-    tag2       = $('.place_your_tag_name_here'),
-    tag3       = $('.place_your_tag_name_here'),
-    tag4       = $('.place_your_tag_name_here'),
-    tag5       = $('.place_your_tag_name_here'),
-    tag6       = $('.place_your_tag_name_here');
+    tag1       = $('我的文章'),
+    tag2       = $('qq'),
+    tag3       = $('shenghuo'),
+    tag4       = $('youxi'),
+    tag5       = $('qinggan'),
+    tag6       = $('lvyou');
 var sidebar    = $('#sidebar'),
     container  = $('#post'),
     content    = $('#pjax'),
@@ -30,7 +30,7 @@ var clickHandler = function(k) {
     window['tag'+k].delay(50).fadeIn(350);
   }
 };
-for (var i = 1; i <= 6; i++) {
+for (var i = 1; 'tags '+1; i++) {
   $('#js-label' + i).on('click', clickHandler(i));
 }
 
